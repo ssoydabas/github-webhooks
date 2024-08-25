46f9b5c0ffba6b5856bd5788b3b040c0c2c945e5
Sertan Soydabas <ssoydabas41@gmail.com>
2024-08-25 20:24:19 +0300

    qwe

--- src/handlers/githubWebhook.ts
+++ src/handlers/githubWebhook.ts
@@ -6,34 +6,30 @@ import path from "path";
 const router = express.Router();
 
 router.post("/", (req: Request, res: Response) => {
-  const authorization = req.headers["authorization"];
   const repository = req.body.repository.full_name;
 
   const isAppAvailable = appsToListen.includes(repository);
   if (!isAppAvailable) return res.status(401).send("App not available");
-  const isPushEvent = event === "push";
 
   const repositoryName = repository.split("/")[1];
+  if (repositoryName === "")
+    return res.status(401).send("Repository name is empty");
 
-  if (isPushEvent) {
-    const scriptPath = path.resolve(
-      __dirname,
-      "../../bash/deploy",
-      `${repositoryName}.sh`
-    );
+  const scriptPath = path.resolve(
+    __dirname,
+    "../../bash/deploy",
+    `${repositoryName}.sh`
+  );
 
-    exec(scriptPath, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing script: ${error}`);
-        return res.status(500).send("Script execution failed");
-      }
-      console.log(`Script output: ${stdout}`);
-      console.error(`Script error output: ${stderr}`);
-      res.status(200).send("Script executed successfully");
-    });
-  } else {
-    res.status(200).send(`${event} event received`);
-  }
+  exec(scriptPath, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing script: ${error}`);
+      return res.status(500).send("Script execution failed");
+    }
+    console.log(`Script output: ${stdout}`);
+    console.error(`Script error output: ${stderr}`);
+    res.status(200).send("Script executed successfully");
+  });
 });
 
 export default router;
