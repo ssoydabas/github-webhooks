b2ae2edc290432770843b57e97ecd932d98f3849
Sertan Soydabas <ssoydabas41@gmail.com>
2024-08-25 20:09:16 +0300

    updates the project to use github actions

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const nodeEnv = process.env.NODE_ENV || "development";
 export const port = process.env.PORT;
 export const githubWebhookSecret =
-  process.env.GITHUB_WEBHOOK_SECRET || "your-secret";
+  process.env.GH_ACTIONS_SECRET || "your-secret";
--- src/middlewares/verifySignature.ts
+++ src/middlewares/verifySignature.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import crypto from "crypto";
 import { githubWebhookSecret } from "../config";
 
 export const verifySignature = (
@@ -8,21 +7,17 @@ export const verifySignature = (
   buf: Buffer,
   encoding: string
 ) => {
-  const signature = req.headers["x-hub-signature-256"] as string;
-  if (!signature) {
-    console.error("Signature header missing");
-    res.status(401).send("Signature header missing");
+  const authorization = req.headers["authorization"] as string;
+  if (!authorization) {
+    console.error("Authorization header missing");
+    res.status(401).send("Authorization header missing");
     return;
   }
-
-  const hmac = crypto.createHmac("sha256", githubWebhookSecret);
-  hmac.update(buf);
-  const expectedSignature = `sha256=${hmac.digest("hex")}`;
-
-  if (signature !== expectedSignature) {
-    console.error("Invalid signature");
-    res.status(401).send("Invalid signature");
+  const expectedToken = `Bearer ${githubWebhookSecret}`;
+  if (authorization !== expectedToken) {
+    console.error("Invalid token");
+    res.status(401).send("Invalid token");
   } else {
-    console.log("Signature verified");
+    console.log("Token verified");
   }
 };
